c6ae440cea4c14816641f1c260758b65504a21ac
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2023-10-11 19:57:51 +0330

    [chore] init vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import 'vuetify/styles'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +7,22 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 const app = createApp(App)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
+})
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
